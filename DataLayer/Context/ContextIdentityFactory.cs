ee9c174b4042cf180bce5df94c0f730d6fc33773
Toxix98 <128359759+Toxix98@users.noreply.github.com>
2025-01-05 02:03:04 +0330

    Add project files.

---
+++ DataLayer/Context/ContextIdentityFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using DataLayer.API.Context;
+
+
+namespace Accapt.DataLayer.ContextIdentoty
+{
+    public class ContextIdentityFactory : IDesignTimeDbContextFactory<UserMangerContext>
+    {
+        public UserMangerContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<UserMangerContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("LocalConnectionDB"));
+
+            return new UserMangerContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ DataLayer/Context/UserMangerContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.API.Context
+{
+    public class UserMangerContext : IdentityDbContext<IdentityUser>
+    {
+        public UserMangerContext(DbContextOptions<UserMangerContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>().ToTable("User");
+            builder.Entity<IdentityRole<string>>().ToTable("UserRole");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("UserRoleClaims");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+        }
+    }
+}
